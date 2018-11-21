2821695f9232f3d0e6eaf9446b4f8c047fd2ce9f
Anshul Goel <anshul@zenabi.com>
2018-11-21 00:09:31 -0500

    chore(cors): added cors support

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const PORT = process.env.PORT || 8888;
 const app = express();
 app.use(bodyParser.json());
+app.use(cors());
 
 const jwt = require('jsonwebtoken');
 const users = require('./users');
