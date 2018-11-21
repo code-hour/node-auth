4c75e6ea4bc90456496c42d7df764a3ce233e287
Anshul Goel <anshul@zenabi.com>
2018-11-21 00:06:56 -0500

    chore(init): initialize the express app with jwt supported

---
+++ src/index.js
@@ -0,0 +1,53 @@
+require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const PORT = process.env.PORT || 8888;
+const app = express();
+app.use(bodyParser.json());
+
+const jwt = require('jsonwebtoken');
+const users = require('./users');
+
+app.get('/', (req, res) => {
+  res
+  .status(200)
+  .send('Application Running');
+});
+
+app.post('/login', (req, res) => {
+  const userName = req.body.userName;
+  const password = req.body.password;
+
+  if (!userName || !password) {
+    res
+    .status(400)
+    .send(`Username or Password missing/incorrect`);
+
+    return;
+  }
+
+  const userAccount = users.find((user) => {
+    return user.user === userName && user.password === password;
+  });
+
+  if (!userAccount) {
+    res.status(401).send(`User not found`);
+    return;
+  }
+
+  const token = jwt.sign({
+    sub: userAccount.id,
+    username: userAccount.user,
+  }, 'mysupersecretkey', {expiresIn: '30 minutes'});
+
+  res.status(200).send({token});
+});
+
+app.get('*', (req, res) => {
+  res.sendStatus(404);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
---
+++ src/users.js
@@ -0,0 +1,17 @@
+module.exports = [{
+  id: 1,
+  user: 'A',
+  password: 'A'
+}, {
+  id: 2,
+  user: 'B',
+  password: 'B'
+}, {
+  id: 3,
+  user: 'C',
+  password: 'C'
+}, {
+  id: 4,
+  user: 'D',
+  password: 'D'
+}];
